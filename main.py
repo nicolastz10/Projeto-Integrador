42e53adb06fa6a72e85201373146259066fcd421
nicolastz10 <142109256+nicolastz10@users.noreply.github.com>
2023-09-30 16:01:05 -0300

    primeiro commt

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+##Tarefacil
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+dias_da_semana = {
+    'segunda feira': [],
+    'terca feira': [],
+    'quarta feira': [],
+    'quinta feira': [],
+    'sexta feira': [],
+    'sabado': [],
+    'domingo': []
+}
+
+organizadorUser = []
+
+def registerUser():
+    UserName = input("Digite seu nome completo:")
+    UserEmail = input("Digite seu E-mail:")
+    UserPassword = input("Crie sua senha:")
+
+    newUser = {}
+    newUser['Name'] = UserName
+    newUser['Email'] = UserEmail
+    newUser['Password'] = UserPassword
+
+    organizadorUser.append(newUser)
+
+def adicionar_tarefa(dia, tarefa):
+    if dia.lower() in dias_da_semana:
+        dias_da_semana[dia.lower()].append(tarefa)
+
+    else:
+        print("Dia da semana inadequado.")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def minha_rotina(dia):
+    if dia.lower() in dias_da_semana:
+        tarefas = dias_da_semana[dia.lower()]
+        if tarefas:
+            print("Tarefas para {dia}:")
+            contador = 1
+
+            for tarefa in tarefas:
+                print(contador, ") ", tarefa)
+                contador += 1
+
+        else:
+            print("Não há tarefas agendadas para {dia}.")
+    else:
+        print("Dia da semana inadequado!")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    print("\nOrganizador de rotina ")
+    print('1 Cadastrar Usuario')
+    print("2. Adicionar tarefa")
+    print("3. Mostrar rotina de um dia")
+    print("4. Sair")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    Option = int(input("Digite sua opção:"))
+
+    if Option == 1:
+        registerUser()
+
+    if Option == 2:
+        dia = input("Digite o dia da semana para adicionar a tarefa: ")
+        tarefa = input("Digite sua tarefa:")
+        adicionar_tarefa(dia, tarefa)
+
+    if Option == 3:
+        dia = input("Digite o dia da semana para mostrar a rotina:")
+        minha_rotina(dia)
+
+    if Option == 4:
+        print("Obrigado meu nobre.")
+        break
+
+
